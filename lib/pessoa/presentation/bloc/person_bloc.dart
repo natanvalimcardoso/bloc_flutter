d09427f7b6739e193916864901aa7318c8cc885e
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-05-26 14:51:09 -0300

    ready

--- lib/pessoa/presentation/bloc/person_bloc.dart
+++ lib/pessoa/presentation/bloc/person_bloc.dart
@@ -1,41 +1,40 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../domain/model/person_model.dart';
 
+import '../../domain/model/person_model.dart';
 import 'person_event.dart';
 import 'person_state.dart';
 
 class PersonBloc extends Bloc<PersonEvent, PersonState> {
   PersonBloc() : super(InitialPersonState()) {
     on<FetchPeopleEvent>(_fetchPeople);
-    on<CreatePersonEvent>(_createNewPerson);
+    // on<CreatePersonEvent>(_createNewPerson);
   }
 
   Future<void> _fetchPeople(event, emit) async {
     emit(LoadingPersonState());
     try {
       final people = await Future.delayed(
-        // o future.delayed é só pra simular caso fosse uma chamada de api
         const Duration(seconds: 1),
         () {
           return _fetchPeopleFunction();
         },
       );
+      print(people);
       emit(SucessPersonState(people));
     } catch (e) {
-      emit(ExceptionPersonState(e.toString(),));
+      emit(ExceptionPersonState(
+        e.toString(),
+      ));
     }
   }
 
-  Future<void> _createNewPerson(CreatePersonEvent event, emit) async {
-    final model = event.model;
-    emit(LoadingPersonState());
-    await _createNewPersonFunction(model);
-    add(FetchPeopleEvent()); //* depois de criar uma pessoa ele vai chamar o estado de  _fetchPeople ou seja mostrar as pessoas
-  }
-}
-
-
-
+//   Future<void> _createNewPerson(CreatePersonEvent event, emit) async {
+//     final model = event.model;
+//     emit(LoadingPersonState());
+//     await _createNewPersonFunction(model);
+//     add(FetchPeopleEvent()); //* depois de criar uma pessoa ele vai chamar o estado de  _fetchPeople ou seja mostrar as pessoas
+//   }
+// }
 
 //------------------------ User Case ---------------------------------//
 
@@ -44,16 +43,15 @@ class PersonBloc extends Bloc<PersonEvent, PersonState> {
 List<PersonModel> _fetchPeopleFunction() {
   List<PersonModel> people = [
     PersonModel(name: 'João', email: 'hoao@hotmaild.com', phone: '989883823'),
-    PersonModel(
-        name: 'Maria', email: 'maria@hotmail.com', phone: '99832322323'),
+    PersonModel(name: 'Maria', email: 'maria@hotmail.com', phone: '99832322323'),
     PersonModel(name: 'José', email: 'Jose@hotmail.com', phone: '99832322323'),
   ];
   return people;
 }
 
-List<PersonModel> _createNewPersonFunction(PersonModel model) {
-  List<PersonModel> people = [
-    PersonModel(name: model.name, email: model.email, phone: model.phone),
-  ];
-  return people;
-}
+// List<PersonModel> _createNewPersonFunction(PersonModel model) {
+//   List<PersonModel> people = [
+//     PersonModel(name: model.name, email: model.email, phone: model.phone),
+//   ];
+//   return people;
+ }
--- lib/pessoa/presentation/pages/home_page.dart
+++ lib/pessoa/presentation/pages/home_page.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_petize/pessoa/presentation/bloc/person_event.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+import '../bloc/person_bloc.dart';
+import '../bloc/person_state.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final bloc = PersonBloc()..add(FetchPeopleEvent());
+  
   @override
   Widget build(BuildContext context) {
-    return Material(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bloc"),
+      ),
+      body: BlocBuilder<PersonBloc, PersonState>(
+        bloc: bloc,
+        builder: (context, state) {
+          if (state is LoadingPersonState) {
+            return const CircularProgressIndicator();
+          }
+
+          if (state is InitialPersonState) {
+            return const Center(
+              child: Text('Nenhuma Pessoa'),
+            );
+          }
+
+          if (state is ExceptionPersonState) {
+            return Center(
+              child: Text(state.message),
+            );
+          }
+
+          if (state is SucessPersonState) {
+            final peopleList = state.models;
+            return ListView.builder(
+              itemCount: peopleList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(peopleList[index].name),
+                  subtitle: Text(peopleList[index].email),
+                  trailing: Text(peopleList[index].phone),
+                );
+              },
+            );
+          }
+
+          return Container();
+        },
+      ),
     );
   }
 }
